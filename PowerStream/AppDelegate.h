334ec305ab1d747c6ae4fe62731f2f4edb202db0
Simon Leigh <simon.leigh13@imperial.ac.uk>
2014-10-30 14:02:13 +0000

    Initial Commit

---
+++ PowerStream/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PowerStream
+//
+//  Created by Simon on 30/10/2014.
+//  Copyright (c) 2014 SJL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
